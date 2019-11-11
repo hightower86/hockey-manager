b77658fc71fe388948324618fc7575823254491e
Adel <htwr86@gmail.com>
2019-11-11 23:25:40 +0300

    hashing password and check existing user

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,11 +11,21 @@ router.post('/', async (req, res) => {
   const { name, email, password } = req.body;
   console.log(name, email, password);
   try {
-    const user = new User({
+    // Check if user already exists in db
+    let user = User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: 'user already exists' }] });
+    }
+    user = new User({
       name,
       email,
       password
     });
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
     console.log(user);
     await user.save();
     res.send('ok');
