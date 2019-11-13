61b7553e9d8564d66967bc5240ee5f3660dc9303
Adel <htwr86@gmail.com>
2019-11-13 20:54:53 +0300

    auth post request

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,19 +1,30 @@
 const router = require('express').Router();
-const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const User = require('../../model/User');
-
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth.js');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../model/User');
 
 // @ GET api/auth
 // @ desc  user login
 // @ access Public
-router.get('/', auth, (req, res) => {
-  res.send('Auth route');
+router.get('/', auth, async (req, res) => {
+  console.log(req.user);
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    console.log(user);
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+  //res.send('Auth route');
 });
 
 // @ POST api/auth
-// @ desc  user login
+// @ desc  authenticate user and get token
 // @ access Public
 router.post(
   '/',
@@ -29,18 +40,45 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-
     const { email, password } = req.body;
-    console.log(email, password);
-    // Check if user already exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.send(400).json({ errors: [{ msg: 'user does not exists' }] });
+
+    try {
+      // Check if user already exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+      //console.log(user);
+
+      // Encrypt and check password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtsecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
-
-    // Encrypt and check password
-
-    res.json(req.body);
   }
 );
 
